6f8a0b5b2d559a61265663e6c79e0a81c3ad808a
Brad Lange <blange@gulllakecs.org>
2022-02-10 14:00:17 -0500

    Import Colorama to change the color of my text

--- Welcome_InfoTechCenter.py
+++ Welcome_InfoTechCenter.py
@@ -2,6 +2,12 @@
 
 from time import sleep  #Print to one line with time delay between prints
 
-print("Welcome to Hornets InfoTechCenter\n") ; sleep(1)
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(strip=False, autoreset=True)
 
-print("Hornet's Operating System Booting Up")
+print(Fore.BLUE + "Welcome to Hornets InfoTechCenter\n")
+
+sleep(1)
+
+print(Fore.BLUE + "Hornet's Operating System Booting Up\n")
