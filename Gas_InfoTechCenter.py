64ff2594cf5308472eefa1e7af70d685c241eb47
Brad Lange <blange@gulllakecs.org>
2022-02-14 14:04:13 -0500

    gasLevelGauge defined and stable

---
+++ Gas_InfoTechCenter.py
@@ -0,0 +1,11 @@
+import random
+
+# Gas Level Function
+def gasLevelGauge():
+    gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+    currentGasLevel = random.choice(gasLevelList)
+    print(currentGasLevel)
+
+gasLevelGauge()
+
+
